7b9bf00b16e35e5251d2abdbe86eb63fc7bf6445
周子龙 <zhouzilong@promote.cache-dns.local>
2020-03-07 10:37:42 +0800

    2020.03.07

--- pro 1/7.1.py
+++ pro 1/7.1.py
@@ -1,5 +1,9 @@
-x = int(input())
-if x == 0:
-    print(f"f({x:.1f}) = 0.0")
-else :
-    print(f"f({x:.1f}) = {1/x:.1f}")
+A, B, C = input().split()
+A, B, C = int(A), int(B), int(C)
+if A > B:
+    A, B = B, A
+if A > C:
+    A, C = C, A
+if B > C:
+    B, C = C, B
+print(f"{A}->{B}->{C}")
